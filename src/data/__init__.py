b8cad6dc02bd40d7ea6268847e69a02f09be093c
Bolgov Alexandr Sergeevich <asbolgov@edu.hse.ru>
2026-05-15 14:13:20 +0300

    add packed sequence data loader

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -5,9 +5,11 @@ except ImportError:
 
 CachedDatasetStats = refinedweb.CachedDatasetStats
 LocalJsonlTextDataset = refinedweb.LocalJsonlTextDataset
+PackedCausalLMDataset = refinedweb.PackedCausalLMDataset
 RefinedWebCacheConfig = refinedweb.RefinedWebCacheConfig
 RefinedWebStreamingDataset = refinedweb.RefinedWebStreamingDataset
 TokenizedCausalLMDataset = refinedweb.TokenizedCausalLMDataset
+build_packed_attention_mask = refinedweb.build_packed_attention_mask
 build_refinedweb_dataloader = refinedweb.build_refinedweb_dataloader
 cache_refinedweb_subset = refinedweb.cache_refinedweb_subset
 count_cached_dataset = refinedweb.count_cached_dataset
@@ -15,9 +17,11 @@ count_cached_dataset = refinedweb.count_cached_dataset
 __all__ = [
     "CachedDatasetStats",
     "LocalJsonlTextDataset",
+    "PackedCausalLMDataset",
     "RefinedWebCacheConfig",
     "RefinedWebStreamingDataset",
     "TokenizedCausalLMDataset",
+    "build_packed_attention_mask",
     "build_refinedweb_dataloader",
     "cache_refinedweb_subset",
     "count_cached_dataset",
